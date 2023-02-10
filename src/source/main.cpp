59ce19b647d5ef5b57b8a72db94ff3f9eb555c30
Sasha Dell <sahadell@mail.ru>
2023-02-10 18:07:55 +0700

    add user input from the console

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "hello, world\n";
+    vector<string> user_input;
+    string figure;
+    while (getline(cin, figure) && figure != "") {
+        user_input.push_back(figure);
+    }
     return 0;
 }
